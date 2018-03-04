9984fde3814f3432e645fe34b248801df7c41034
B92064 <league@wts-im-6.attlocal.net>
2018-03-03 16:26:54 -0800

    Finished Module 2

--- src/random/Rollercoaster.java
+++ src/random/Rollercoaster.java
@@ -6,6 +6,12 @@ public class Rollercoaster {
 public static void main(String[]args) {
 	String height = JOptionPane.showInputDialog("What's your height?(inches)");
 	int intheight = Integer.parseInt(height);
+	if (intheight <= 48) {
+		JOptionPane.showMessageDialog(null, "You need to grow more first!");
+	}
+	else if(intheight > 48) {
+		JOptionPane.showMessageDialog(null, "You can go on the rollercoaster!");
+	}
 	
 }
 }
